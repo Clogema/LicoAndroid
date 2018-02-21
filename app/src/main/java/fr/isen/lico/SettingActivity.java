33d1bfbb2f41c671dd04ee79f4281824c13a0710
Lisa Anthonioz <lisa.anthonioz@isen.yncrea.fr>
2018-02-21 21:54:11 +0100

    Ajout fonctionnalité Bouton Inutile

--- app/src/main/java/fr/isen/lico/SettingActivity.java
+++ app/src/main/java/fr/isen/lico/SettingActivity.java
@@ -1,13 +1,15 @@
 package fr.isen.lico;
 
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -30,6 +32,8 @@ public class SettingActivity extends AppCompatActivity {
 
         final TextView tvNbjoueur = findViewById(R.id.tvNbjoueur);
 
+        final Switch sUseless = (Switch) findViewById(R.id.sUseless);
+
         /***** Récupération des données *****/
         Intent intent = getIntent();
         nbJoueur = intent.getIntExtra("nbJoueur", nbJoueur);
@@ -46,6 +50,26 @@ public class SettingActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 SettingActivity.this.finish();
+            }
+        });
+
+        /***** Test Bouton Inutile *****/
+        sUseless.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                android.app.AlertDialog.Builder alertDialogBuilder = new android.app.AlertDialog.Builder(SettingActivity.this);
+                alertDialogBuilder.setTitle("Félicitation !");
+                alertDialogBuilder.setMessage("Bravo pour ta découverte, tu peux boire 2 gorgées !");
+                alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int arg1) {
+                        dialog.cancel();
+                    }
+                });
+
+                android.app.AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+
+                sUseless.setChecked(false);
             }
         });
     }
